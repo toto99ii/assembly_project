2eabcf0b5c2d3fde1963146617171d9f5ae3428c
toto99ii <omertoto99ii@gmail.com>
2021-12-26 21:59:43 +0200

    Create assembly_code.asm

---
+++ assembly_code.asm
@@ -0,0 +1,268 @@
+IDEAL
+
+MODEL small
+
+STACK 256
+
+DATASEG
+x dw 158
+y dw 199
+color db 15
+
+CODESEG
+proc enter_graphic_mode
+    mov ax, 13h
+    int 10h
+    ret
+endp
+
+proc print_dot_from_memory
+    push bx
+    push cx
+    push dx
+
+    mov bh, 0h 
+    mov cx, [x] 
+    mov dx, [y] 
+    mov al, [color] 
+    mov ah, 0ch 
+    int 10h
+
+    pop dx
+    pop cx
+    pop bx
+    ret
+endp
+proc color_pixel
+    call enter_graphic_mode
+
+next:
+    call print_dot_from_memory
+
+    ret
+endp
+
+proc print_man
+    call enter_graphic_mode
+    push bx
+    push cx
+    push dx
+
+    mov bh, 0h 
+    mov cx, [x]
+    mov dx, [y]
+    mov al, [color] 
+    mov ah, 0ch 
+    int 10h
+    add cx, 1
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    add cx, 1
+    mov ah, 0ch 
+    int 10h
+    add cx, 1
+    mov ah, 0ch 
+    int 10h
+    add dx, 1
+    mov ah, 0ch 
+    int 10h
+    add dx, 1
+    mov ah, 0ch 
+    int 10h
+    add dx, 1
+    mov ah, 0ch 
+    int 10h
+    add dx, 1
+    mov ah, 0ch 
+    int 10h
+    add cx, 1
+    mov ah, 0ch 
+    int 10h         ;finish legs
+    sub dx, 5
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub cx, 1
+    mov ah, 0ch 
+    int 10h
+    sub cx, 1
+    mov ah, 0ch 
+    int 10h
+    sub cx, 1
+    mov ah, 0ch 
+    int 10h
+    sub cx, 1
+    add dx, 1
+    mov ah, 0ch 
+    int 10h
+    add dx, 1
+    mov ah, 0ch 
+    int 10h
+    add dx, 1
+    mov ah, 0ch 
+    int 10h
+    add dx, 1
+    mov ah, 0ch 
+    int 10h                     ;finish body
+    add cx, 6
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub cx, 1
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub cx, 5
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub cx, 1
+    mov ah, 0ch 
+    int 10h
+    sub cx, 1
+    mov ah, 0ch 
+    int 10h
+    sub cx, 1
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub cx, 1
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    add cx, 3
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    add cx, 1
+    mov ah, 0ch 
+    int 10h
+    sub dx, 1
+    add cx, 1
+    mov ah, 0ch 
+    int 10h
+    add cx, 1
+    mov ah, 0ch 
+    int 10h
+    add cx, 1
+    mov ah, 0ch 
+    int 10h
+    add cx, 1
+    add dx, 1
+    mov ah, 0ch 
+    int 10h
+    add cx, 1
+    add dx, 1
+    mov ah, 0ch 
+    int 10h
+    add dx, 1
+    mov ah, 0ch 
+    int 10h
+    add dx, 1
+    mov ah, 0ch 
+    int 10h
+    add dx, 1
+    sub cx, 1
+    mov ah, 0ch 
+    int 10h
+    add dx, 1
+    sub cx, 1
+    mov ah, 0ch 
+    int 10h
+    sub cx, 1
+    mov ah, 0ch 
+    int 10h
+    sub cx, 1
+    mov ah, 0ch 
+    int 10h
+    sub cx, 1
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    sub cx, 1
+    sub dx, 1
+    mov ah, 0ch 
+    int 10h
+    add cx, 2
+    mov ah, 0ch 
+    int 10h
+    add cx, 1
+    mov ah, 0ch 
+    int 10h
+    add cx, 1
+    mov ah, 0ch 
+    int 10h
+    sub dx, 2
+    mov ah, 0ch 
+    int 10h
+    sub cx, 2
+    mov ah, 0ch 
+    int 10h
+    add cx, 1
+    add dx, 5
+    mov ah, 0ch 
+    int 10h
+    add dx, 10
+    sub cx, 2
+
+    pop dx
+    pop cx
+    pop bx
+    ret
+endp
+
+
+Start:
+    mov ax, @data
+    mov ds, ax
+
+    call print_man
+    mov ah, 07h
+    int 21h
+
+
+Exit:
+    mov ax, 4C00h
+    int 21h
+END start
+
