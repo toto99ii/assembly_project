41cc11766b27bbaec7e3a13d41e2514485fe4733
toto99ii <omertoto99ii@gmail.com>
2022-02-02 10:05:45 +0200

    coins almost finished
    coins are now falling from the sky
    when pressing space game starts, you cant move before
    when coins hit u a new coin generate
    when coins hit the ground a new one generate

--- assembly_code.asm
+++ assembly_code.asm
@@ -29,6 +29,10 @@ coin1 dw 00h, 00h, 00h, 00h, 00h, 02h, 02h, 02h, 02h, 02h, 00h, 00h, 00h, 00h, 0
 xcoin dw 0
 timer dw 0
 game_started db 0
+hight dw 0
+coin_active db 0
+color_coin db 0
+touch_check dw 0
 
 CODESEG
 proc enter_graphic_mode
@@ -254,14 +258,13 @@ proc print_man
 endp
 
 proc draw_coin
-    call rand_pos
     push bx
     push cx
     push dx
     mov cx, [xcoin]
     mov bh, 0h
-    mov dx, 0
-    mov al, 14
+    mov dx, [hight]
+    mov al, [color_coin]
     mov ah, 0ch
     int 10h
     add cx, 1
@@ -766,7 +769,7 @@ endp
 proc move_left
     mov [color], 0
     call print_man
-    dec [loc_x]
+    sub [loc_x], 3
     mov [color], 15
     call print_man
     ret
@@ -775,20 +778,25 @@ endp
 proc move_right
     mov [color], 0
     call print_man
-    inc [loc_x]
+    add [loc_x], 3
     mov [color], 15
     call print_man
     ret
 endp
 
 proc coins_generator
-    add [timer], 1
-    cmp [timer], 50
-    je coin
-    ret
-coin:
+    call rand_pos
+    mov [hight], 0
+    mov [coin_active], 1
+    mov [color_coin], 14
     call draw_coin
-    mov [timer], 0
+    ret
+endp
+
+proc touch
+    mov [color_coin], 0
+    call draw_coin
+    mov [coin_active], 0
     ret
 endp
 
@@ -802,26 +810,57 @@ hey:
     mov al, 0h
     mov ah, 07h
     int 21h
+    cmp al, " "
+    je game
+    cmp [game_started], 0
+    je hey
     cmp al, "a"
     je left
     cmp al, "d"
     je right
-    cmp al, " "
-    je game
-    cmp [game_started], 1
-    je generate_coin
-    jmp hey
+one:
+    cmp [coin_active], 1
+    je move_coin
+    jmp generate_coin
 left:
     call move_left
-    jmp hey
+    jmp one
 right:
     call move_right
-    jmp hey
+    jmp one
 game:
     mov [game_started], 1
     jmp hey
+move_coin:
+    mov [color_coin], 0
+    call draw_coin
+    add [hight], 1
+    mov [color_coin], 14
+    call draw_coin
+    cmp [hight], 195
+    jge lose
+    cmp [hight], 160
+    jge may_touch
+    jmp hey
+lose:
+    mov [color_coin], 0
+    call draw_coin
+    mov [coin_active], 0
+    jmp hey
+may_touch:
+    mov ax, [loc_x]
+    mov [touch_check], ax
+    mov ax, [xcoin]
+    sub [touch_check], ax
+    cmp [touch_check], 10
+    jg hey
+    cmp [touch_check], -10
+    jl hey1
+    call touch
 generate_coin:
     call coins_generator
+    jmp hey
+hey1:
     jmp hey
 Exit:
     mov ax, 4C00h
