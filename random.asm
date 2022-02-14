74e94c61f6cc82140dc0c8b90a6ea860272b5f99
toto99ii <omertoto99ii@gmail.com>
2022-02-14 10:15:28 +0200

    Update random.asm

--- random.asm
+++ random.asm
@@ -6,67 +6,31 @@ STACK 256
 
 DATASEG
 
-NextRandom dw 0
-j dw 0
-
+sound_timer dw 0
 CODESEG
-
-proc prg
-    push dx
-    xor dx, dx
-    mov ax, [NextRandom]
-    mov dx, 25173
-    imul dx
-    add  ax, 13849
-    xor  ax, 62832
-    mov  [NextRandom], ax
-    pop dx
-    ret
-endp prg
-
-
-proc print
-	mov cx,0
-	mov dx,0
-label1:
-	cmp ax,0
-	je print1
-	mov bx,10
-	div bx
-	push dx
-	inc cx
-	xor dx,dx
-	jmp label1
-print1:
-	cmp cx,0
-	je exit
-	pop dx
-	add dx,48
-	mov ah,02h
-	int 21h
-	dec cx
-	jmp print1
-exit:
-   ret
-endp
 
 Start:
     mov ax, @data
     mov ds, ax
-    jmp hey
 
-hey:
-    add [j], 1
-    cmp [j], 50
-    je coin
-    jmp hey
-coin:
-    mov [ax], 0
-    jmp hey
+    mov [sound_timer], 100
+    mov cx, 500h
+    mov bx, 9999h
+    mov ax, 1521
 
-Exit:
-    mov ax, 4C00h
-    int 21h
+    mov al, 182
+    out 43h, al
+    mov ax, cx
+
+    out 42h, al
+    mov al, ah
+    out 42h, al
+    in  al, 61h
+
+    or al, 00000011b
+    out 61h, al
+
+exit:
+    dec [sound_timer]
+    jne exit
 END start
-
-
